bf37ee8b4af018bf400a2d80d89edae220e51aea
Sujal Maurya <sujalmaurya08@gmail.com>
2026-07-22 17:34:35 +0530

    Time: 3 ms (88.71%), Space: 65.8 MB (42.93%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -11,7 +11,24 @@
  */
 class Solution {
 public:
+void inorder(TreeNode* r,vector<int>&in){
+    if(r==NULL) return;
+    inorder(r->left,in);
+    in.push_back(r->val);
+    inorder(r->right,in);
+}
+TreeNode* make(vector<int>&in,int i,int j){
+    if(i>j) return NULL;
+    int mid=(i+j)/2;
+    TreeNode* r=new TreeNode(in[mid]);
+    r->left=make(in,i,mid-1);
+    r->right=make(in,mid+1,j);
+    return r;
+
+}
     TreeNode* balanceBST(TreeNode* root) {
-        
+        vector<int> in;
+        inorder(root,in);
+        return make(in,0,in.size()-1);
     }
 };
