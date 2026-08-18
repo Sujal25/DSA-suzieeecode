5a3b019a3a94721d01b686a1f93929cf4fd5dfa9
Sujal Maurya <sujalmaurya08@gmail.com>
2026-08-18 19:42:20 +0530

    Time: 0 ms (100%), Space: 10.8 MB (24.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -14,12 +14,22 @@ int chk(vector<int>&nums,vector<int>&dp,int ind){
     int rob(vector<int>& nums) {
 
         int n=nums.size();
-        vector<int> dp1(n,-1);
-       vector<int> dp2(n,-1);
-       return max(chk(nums,dp1,n-1),chk(nums,dp2,n-2));
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        vector<int> dp(n,-1);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+       //vector<int> dp2(n,-1);
+       //return max(chk(nums,dp1,n-1),chk(nums,dp2,n-2));
+       for(int i=2;i<n;i++){
+        dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+       }
+       return dp[n-1];
     }
 };
-/*void chk(vector<int>&nums,int ind,int b){
+/*
+recursive approach 
+void chk(vector<int>&nums,int ind,int b){
     if(ind<0) {
         ans=max(ans,b);
         return;}
@@ -33,4 +43,19 @@ int chk(vector<int>&nums,vector<int>&dp,int ind){
        chk(nums,n-2,0);
        return ans;
     }
-};*/
+};
+
+memoization 
+int chk(vector<int>&nums,vector<int>&dp,int ind){
+    if(ind<0) {
+        
+        return 0; }
+        if(dp[ind]!=-1) return dp[ind];
+        int robb=chk(nums,dp,ind-1);
+        int nrobb=nums[ind]+chk(nums,dp,ind-2);
+        return dp[ind]=max(robb,nrobb);
+
+}
+
+
+*/
