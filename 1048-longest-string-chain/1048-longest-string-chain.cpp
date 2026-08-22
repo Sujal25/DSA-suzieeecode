c4422108b7ec15e8ecf8cdc0dd8269c07461ba19
Sujal Maurya <sujalmaurya08@gmail.com>
2026-08-22 17:14:46 +0530

    Time: 51 ms (49.71%), Space: 32.3 MB (35.55%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -27,3 +27,32 @@ for(int i=0;i<words.size();i++){
 return ans;
     }
 };
+/*class Solution {
+public:
+unordered_map<string,int> mp;
+int ans=1;
+void chk(string s,int l){
+    for(int i=0;i<s.size();i++){
+        string k = s;
+k.erase(i, 1);
+if(mp.find(k)!=mp.end()) {chk(k,l+1);
+ ans=max(ans,l);}
+    }
+        return ;
+    
+}
+    int longestStrChain(vector<string>& words) {
+      
+      for(auto &s:words) mp[s]++;
+      int len=1;
+      sort(words.begin(),words.end(),[](const string a,const string b){
+        return a.size()>b.size();
+      });
+
+for(int i=0;i<words.size();i++){
+    chk(words[i],2);
+}
+if(ans==0) return 1;
+return ans;
+    }
+};*/
