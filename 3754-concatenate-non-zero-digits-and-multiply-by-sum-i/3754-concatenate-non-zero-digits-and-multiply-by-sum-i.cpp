bd2b089b8f855070aa2284e7361d67821440318e
Sujal Maurya <sujalmaurya08@gmail.com>
2026-09-05 22:21:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        if(n==0) return 0;
-        long long ans=0;
-        string p=to_string(n);
-        string a="";
-        int sum=0;
-        for(char c:p){
-            if(c=='0') continue;
-            else{
-                a+=c;
-                sum+=(c-'0');
-            }
+        string k="";
+        int s=0;
+        string b=to_string(n);
+        for(char c:b){
+            s+=(c-'0');
+            if(c!='0') k+=c;
         }
-        return stoll(a)*sum;
+        return stol(k)*s;
     }
 };
