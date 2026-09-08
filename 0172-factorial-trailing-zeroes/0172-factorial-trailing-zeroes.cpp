64e5a482f590a15b077d5c87a7b0c1a2627e8ea7
Sujal Maurya <sujalmaurya08@gmail.com>
2026-09-08 20:28:27 +0530

    Time: 4 ms (3.15%), Space: 8.1 MB (50.5%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -3,26 +3,19 @@ public:
 
     int trailingZeroes(int n) {
         // basiclly how many factor of 10 are ther 
-        int two=0;
+    
         int five=0;
         int ten=0;
         for(int i=n;i>0;i--){
             int k=i;
-            while(k%2==0){
-                two++;
-                k/=2;
-            }
-
+           
             while(k%5==0){
                 five++;
                 k/=5;
             }
-            while(k%10==0){
-                ten++;
-                k/=10;
-            }
+            
             
         }
-return min(five,two)+ten;
+return five;
     }
 };
