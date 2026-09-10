d73e5def835b45c769a07ebee81ce702ddb2d7cd
Sujal Maurya <sujalmaurya08@gmail.com>
2026-09-10 14:25:57 +0530

    Time: 10 ms (39.19%), Space: 15.8 MB (64.72%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -12,20 +12,25 @@
 class Solution {
 public:
 int ans=0;
-int cnt(TreeNode* r) {
-        if (!r) return 0;
-        return 1 + cnt(r->left) + cnt(r->right);
-    }
-int avg(TreeNode* r){
-    if(!r) return 0;
-    int l=avg(r->left);
-    int rt=avg(r->right);
-   
-    if(r->val==(l+rt+r->val)/cnt(r)) ans++;
-    return l+rt+r->val;
+int sub(TreeNode* r,int& b){
+    if(!r) {
+
+        return 0;}
+        int lb=0;
+        int rb=0;
+       
+    int rt=sub(r->left,lb);
+    int lt=sub(r->right,rb);
+    b=lb+rb+1;
+    if(r->val==(rt+lt+r->val)/(b)) ans++;
+    return rt+lt+r->val;
+
+
 }
+//sub of subroot 
     int averageOfSubtree(TreeNode* root) {
-        avg(root);
+        int b=0;
+        sub(root,b);
         return ans;
     }
 };
